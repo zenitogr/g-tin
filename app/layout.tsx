0d2f72241e1af08df71120a6ba9b2075d5f0f921
ZenitoGR <zengodgr@outlook.com>
2024-10-13 01:44:16 +0300

    fixed bottom nav bar not showing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import DynamicNav from '@/components/DynamicNav';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
         <main className="flex-grow overflow-hidden flex flex-col">
           {children}
         </main>
+        <DynamicNav />
       </body>
     </html>
   );
